0c1c15a11f9f4f855ebb112b984cb968a1b1e87d
boydjohnson <lt359mn38>
2015-05-20 17:09:24 -0500

    Callable for is_current that shows up in changelist

--- versions/admin.py
+++ versions/admin.py
@@ -13,14 +13,18 @@ class VersionableAdmin(ModelAdmin):
     readonly_fields = ('id','identity')
 
 
+
     def get_readonly_fields(self, request, obj=None):
         """this method is needed so that if a subclass of VersionableAdmin has readonly_fields the
                 the ones written above won't be undone"""
         if obj:
-            return self.readonly_fields + ('id','identity')
+            return self.readonly_fields + ('id','identity','is_current')
         return self.readonly_fields
 
-
+    def get_list_display(self, request):
+        """this method determines which fields go in the changelist"""
+        f = list(super(VersionableAdmin,self).get_list_display(request))
+        return f + ['is_current']
 
 
     def save_model(self, request, obj, form, change):
@@ -31,3 +35,9 @@ class VersionableAdmin(ModelAdmin):
             newer_object.name = form.cleaned_data["name"]
             newer_object.age = form.cleaned_data['age']
             newer_object.save()
+
+
+    def is_current(self, obj):
+        return obj.is_current
+
+    is_current.boolean = True
