8e4b93610ac80fc1c467a6974afbf48d32ee02c2
Brian King <brian@liip.ch>
2015-06-05 15:41:15 +0100

    add versions.settings.get_setting as a generic way to get overrideable defaults

--- versions/settings.py
+++ versions/settings.py
@@ -17,13 +17,13 @@ def import_from_string(val, setting_name):
             (val, setting_name, e.__class__.__name__, e)))
 
 _cache = {}
+_defaults = {
+    'VERSIONED_DELETE_COLLECTOR': 'versions.deletion.VersionedCollector'
+}
+
 def get_versioned_delete_collector_class():
     """
     Gets the class to use for deletion collection.
-
-    This is done as a method instead of just defining a module-level variable because
-    Django doesn't like attributes of the django.conf.settings object to be accessed
-    in top-level module scope.
 
     :return: class
     """
@@ -31,6 +31,25 @@ def get_versioned_delete_collector_class():
     try:
         cls = _cache[key]
     except KeyError:
-        cls = import_from_string(getattr(settings, key, 'versions.deletion.VersionedCollector'), key)
+        collector_class_string = get_setting(key)
+        cls = import_from_string(collector_class_string, key)
         _cache[key] = cls
     return cls
+
+def get_setting(setting_name):
+    """
+    Gets a setting from django.conf.settings if set, otherwise from the defaults
+    defined in this module.
+
+    A function is used for this instead of just defining a module-level variable because
+    Django doesn't like attributes of the django.conf.settings object to be accessed in
+    module scope.
+
+    :param string setting_name: setting to take from django.conf.setting
+    :return: class
+    """
+    try:
+        return getattr(settings, setting_name)
+    except AttributeError:
+        return _defaults[setting_name]
+
