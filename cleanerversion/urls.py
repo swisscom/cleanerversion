88448a357e15fbdcc96aa20118f552a24b19a7e0
Manuel Jeckelmann <mjeckelmann@gmail.com>
2015-06-11 08:24:14 +0200

    PEP8 on some files regarding admin integration

--- cleanerversion/urls.py
+++ cleanerversion/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 from django.contrib import admin
 
 
 urlpatterns = patterns('',
-                url(r'^admin/',admin.site.urls,),)
+                       url(r'^admin/', admin.site.urls, ), )
--- versions_tests/admin.py
+++ versions_tests/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+
 from versions.admin import VersionedAdmin
+from versions_tests.models import City, Student, Observer, Professor, Subject, Teacher, Team, Player, Award, ChainStore, \
+    Classroom, WineDrinker, WineDrinkerHat, Wine
 
-from versions_tests.models import City, Student, Observer, Professor, Subject, Teacher, Team, Player, Award, ChainStore, Classroom, WineDrinker, WineDrinkerHat
 
-
-admin.site.register([City, Student, Subject, Teacher, Team, Player, Award, Observer, ChainStore, Professor, Classroom, WineDrinker, WineDrinkerHat], VersionedAdmin)
+admin.site.register(
+    [City, Student, Subject, Teacher, Team, Player, Award, Observer, ChainStore, Professor, Classroom, Wine,
+     WineDrinker, WineDrinkerHat], VersionedAdmin)
