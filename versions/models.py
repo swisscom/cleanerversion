4d3fc9ee5559e253fb3abbeaa7ab0f416bdd8fa3
boydjohnson <johnson.boyd@gmail.com>
2015-07-13 16:42:23 -0500

    Super simple try; gets required behavior; using Travis tests

--- versions/models.py
+++ versions/models.py
@@ -383,7 +383,7 @@ class VersionedExtraWhere(ExtraWhere):
         if self._joined_alias:
             sql = sql.format(alias=self._joined_alias)
         else:
-            raise ValueError("joined_alias not set")
+            pass
 
         # Set the final sqls
         # self.sqls needs to be set before the call to parent
