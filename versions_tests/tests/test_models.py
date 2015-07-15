5d15da7a92d5542c4327775e50505f229528a1db
Manuel Jeckelmann <mjeckelmann@gmail.com>
2015-07-15 13:56:41 +0200

    Refined test-case and integrated it to an existing test class

--- versions_tests/tests/test_models.py
+++ versions_tests/tests/test_models.py
@@ -2149,6 +2149,15 @@ class IntegrationNonVersionableModelsTests(TestCase):
         should_be_jacques_t1 = should_be_jacques.__class__.objects.as_of(self.t1).get(identity=should_be_jacques.identity)
         self.assertEqual(jacques_t1, should_be_jacques_t1)
 
+    def test_filter_on_fk_versioned_and_nonversioned_join(self):
+        # Get non-versioned objects, filtering on a FK-related versioned object
+        jacques_hats = WineDrinkerHat.objects.filter(wearer__name='Jacques').distinct()
+        self.assertEqual(set(jacques_hats), set([self.green_vagabond_hat, self.red_sailor_hat]))
+
+        # Get all versions of a Versionable by filtering on a FK-related non-versioned object
+        person_versions = WineDrinker.objects.filter(hats__shape='Vagabond')
+        self.assertIn(self.jacques, person_versions)
+
 
 class FilterOnForeignKeyRelationTest(TestCase):
     def test_filter_on_fk_relation(self):
@@ -2161,15 +2170,6 @@ class FilterOnForeignKeyRelationTest(TestCase):
         l2 = len(Player.objects.as_of(t1).filter(team__name='team'))
         self.assertEqual(l1, l2)
 
-    def test_filter_on_fk_versioned_and_nonversioned_join(self):
-        michael = WineDrinker.objects.create(name='michael', glass_content=Wine.objects.create(name='Port',vintage='2013'))
-        baseball_hat = WineDrinkerHat.objects.create(shape='baseball hat', color='blue', wearer=michael)
-        michael.save()
-        baseball_hat.save()
-        hat = WineDrinkerHat.objects.filter(wearer__name='michael').get()
-        self.assertEqual(hat.pk, baseball_hat.pk)
-        person = WineDrinker.objects.filter(hats__shape='baseball hat').get()
-        self.assertEqual(person.pk, michael.pk)
 
 class SpecifiedUUIDTest(TestCase):
 
